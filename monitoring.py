401b0a3523bf5401463ef14c65ebec1dd11917a6
T_O <theogz@users.noreply.github.com>
2016-02-07 03:01:54 +0100

    petits tests

--- monitoring.py
+++ monitoring.py
@@ -1,18 +1,48 @@
 import requests, json, pandas as pd
+from time import sleep
 
 url = 'https://www.autolib.eu/fr/stations'
-r = requests.get(url)
-source_code = str(r.content)
-raw_data = source_code.split("var map = initMap(",1)[1].split('\n', 1)[0][:-3]
 
+def update():
+    r = requests.get(url)
+    source_code = str(r.content)
+    raw_data = source_code.split("var map = initMap(",1)[1].split('\n', 1)[0][:-3]
+    
+    global stations
+    stations = pd.DataFrame(pd.read_json(raw_data))
 
-stations = pd.DataFrame(pd.read_json(raw_data))
-
-stations.set_index('station_id', inplace=True)
+    stations.set_index('station_id', inplace=True)
 
 
 def get_cars(station_id):
+    update()
     return stations.loc[station_id, 'cars']
-print get_cars(60)
-# def get_spots(station_id):
-# def monitoring(station_id, type): #type = depart ou arrivee
+
+def get_parks(station_id):
+    update()
+    return stations.loc[station_id, 'parks']
+
+
+
+def monitoring(station_id, trip):
+    update()
+    if (trip=='depart') and (get_cars(station_id)=0):
+        while (get_cars(stations_id) = 0):
+            sleep(1)
+            update()
+        print r"Une voiture est disponible à la station"
+
+    elif (get_cars(station_id)>0):
+        print r"Il y a déjà une voiture."
+
+    elif (trip=="arrivee") and (get_parks(station_id)=0):
+        while (get_parks(station_id) = 0):
+            sleep(1)
+            update
+        print r"Une place est disponible à la station."
+
+    elif (get_parks(station_id)=0):
+        print r"Il y a déjà une place."
+
+    else:
+        print "Erreur inconnue"
